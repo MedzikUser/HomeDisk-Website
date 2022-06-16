8d3d8dd9254fb7dba2a7bebdc8bffe221493d2fe
MedzikUser <nivua1fn@duck.com>
2022-06-16 14:36:03 +0200

    chore: change title `font-size` to `2rem` (due to mobile broken)

--- components/title.tsx
+++ components/title.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components"
 
 export default styled.h1`
     color: ${({ theme }) => theme.colors.title};
-    font-size: 3rem;
+    font-size: 2rem;
     margin-bottom: 0px;
 `
