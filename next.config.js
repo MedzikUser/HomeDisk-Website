11b05c60de4ed5bfa7ccb7ffca8b3a8b5a5f9239
MedzikUser <nivua1fn@duck.com>
2022-06-21 13:02:46 +0200

    chore: change api requests (https://github.com/MedzikUser/HomeDisk/commit/03afaa1b492847f657bbe6243470f0864e76bc00)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         return [
             {
                 source: '/api/:slug*',
-                destination: 'http://127.0.0.1:8080/:slug*'
+                destination: 'http://127.0.0.1:8080/api/:slug*'
             }
         ]
     }
