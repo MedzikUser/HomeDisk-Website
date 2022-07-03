3cfcef05e2d7895d99a5febb9568eeafc6120d71
MedzikUser <nivua1fn@duck.com>
2022-07-03 16:29:54 +0200

    feat: add 404 and other errors pages

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+  export function load({ error, status }: any) {
+    return {
+      props: {
+        message: error.message,
+        status
+      }
+    }
+  }
+</script>
+
+<script lang="ts">
+  export let status: number
+  export let message: string
+</script>
+
+{#if status == 404}
+  <h1>404</h1>
+  <code>Error: {message}</code>
+{:else}
+  <h1>{status}</h1>
+  <code>Error: A server-side error occurred ({message})</code>
+{/if}
